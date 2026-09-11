f6c1f702cf65696de084ee3389d14e04c8371d53
Ritik7987 <ritik@flipr.ai>
2026-09-11 12:12:13 +0530

    fixed cd build issue

--- jenkins-local/docker-entrypoint.sh
+++ jenkins-local/docker-entrypoint.sh
@@ -6,4 +6,5 @@ if [ -S /var/run/docker.sock ]; then
     chmod 666 /var/run/docker.sock
 fi
 
-exec /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
+# Drop privileges and run Jenkins as the 'jenkins' user
+exec gosu jenkins /usr/bin/tini -- /usr/local/bin/jenkins.sh "$@"
--- vars/ciCdPipeline.groovy
+++ vars/ciCdPipeline.groovy
@@ -3,8 +3,10 @@ def call(Map args = [:]) {
     def config = args.config ?: [:]
     def apps = args.apps ?: []
 
-    def deployComment =
-        env.GITHUB_COMMENT?.trim() == 'DEPLOY'
+    def deployComment = 
+        (env.GITHUB_COMMENT?.trim() == 'DEPLOY') ||
+        (env.ghprbCommentBody?.trim() == 'DEPLOY') ||
+        (env.GITHUB_PR_COMMENT_BODY?.trim() == 'DEPLOY')
 
     if (deployComment) {
 
