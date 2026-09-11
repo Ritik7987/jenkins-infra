23dc0d1ef1dcda47173d0a4c6959bdee221d8599
Ritik7987 <ritik@flipr.ai>
2026-09-11 12:14:27 +0530

    updates

--- vars/ciCdPipeline.groovy
+++ vars/ciCdPipeline.groovy
@@ -3,6 +3,9 @@ def call(Map args = [:]) {
     def config = args.config ?: [:]
     def apps = args.apps ?: []
 
+    // DEBUG: Let's see what environment variables Jenkins actually has!
+    sh 'printenv | sort'
+
     def deployComment = 
         (env.GITHUB_COMMENT?.trim() == 'DEPLOY') ||
         (env.ghprbCommentBody?.trim() == 'DEPLOY') ||
